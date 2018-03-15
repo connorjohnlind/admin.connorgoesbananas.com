11578fae472412d5c6d0c39530fdd424383c2b7d
connorjohnlind <connorjohnlind@gmail.com>
2018-03-15 16:30:27 -0700

    Added POST /auth/renew

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,16 +1,17 @@
+const _ = require('lodash');
 const { User } = require('./../models/user');
 
 const authenticate = async (req, res, next) => { // eslint-disable-line consistent-return
-  const token = req.header('x-auth');
   try {
+    const { token } = _.pick(req.body, ['token']);
     const user = await User.findByToken(token);
     if (!user) return Promise.reject();
     req.user = user;
     req.token = token;
+    next();
   } catch (e) {
     res.status(401).send();
   }
-  next();
 };
 
 module.exports = { authenticate };
--- server.js
+++ server.js
@@ -49,6 +49,21 @@ app.post('/api/post', async (req, res) => {
   }
 });
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+app.post('/auth/renew', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
